32a8a7257dae9329bafa76ac66c813b9322ae46a
byelBorges <gabryelhenborges@gmail.com>
2024-10-12 14:43:21 -0300

    Espressão Regular finalizada

---
+++ proj-simulador-lfa-javafx/src/main/java/unoeste/fipp/gabryelborges/projsimuladorlfajavafx/ExpRegViewController.java
@@ -0,0 +1,78 @@
+package unoeste.fipp.gabryelborges.projsimuladorlfajavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExpRegViewController implements Initializable {
+    private static final String ER_PATTERN = "^[0-9a-zA-Z*+|().ε{},]+$";
+    public TextField expRegField;
+    public TextField entradaField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Inicialização do controlador com a ER básica
+
+    }
+
+    public boolean ehExpRegValida(String er) { // Verifica se a ER é válida
+        return er.matches(ER_PATTERN);
+    }
+
+    public boolean testaExpReg(String er, String input) { // Compila e testa a ER
+        Pattern pattern = Pattern.compile(er);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public void exibirMensagem(String titulo, String headerText, String contentText, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(titulo);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.showAndWait();
+    }
+
+    public void bttTestarEntradaClick(ActionEvent actionEvent) {
+        String er = expRegField.getText();
+        er = er.replace("ε", "");
+        String input = entradaField.getText();
+        if (testaExpReg(er, input)) {
+            exibirMensagem("Entrada aceita", null, "Entrada aceita", Alert.AlertType.INFORMATION);
+        } else {
+            exibirMensagem("Entrada não aceita", null, "Entrada não aceita", Alert.AlertType.ERROR);
+        }
+    }
+
+    public void eventoInputExpReg(KeyEvent keyEvent) {
+        String er = expRegField.getText();
+        if (er.length() > 0) {
+            if (!ehExpRegValida(er)) {
+                exibirMensagem("Expressão inválida", null, "Expressão inválida", Alert.AlertType.ERROR);
+                String invalidChar = keyEvent.getCharacter();
+                expRegField.setText(er.replace(invalidChar, "")); // Remove o caractere inválido
+            }
+        }
+    }
+
+
+
+    public void bttPipelineClick(ActionEvent actionEvent) {
+        expRegField.setText(expRegField.getText() + "|");
+        expRegField.requestFocus();
+        expRegField.positionCaret(expRegField.getText().length());
+
+    }
+
+    public void bttVazioClick(ActionEvent actionEvent) {
+        expRegField.setText(expRegField.getText() + "ε");
+        expRegField.requestFocus();
+    }
+}
--- proj-simulador-lfa-javafx/src/main/java/unoeste/fipp/gabryelborges/projsimuladorlfajavafx/HelloController.java
+++ proj-simulador-lfa-javafx/src/main/java/unoeste/fipp/gabryelborges/projsimuladorlfajavafx/HelloController.java
@@ -1,14 +1,44 @@
 package unoeste.fipp.gabryelborges.projsimuladorlfajavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+    public Button bttExpReg;
+    public Button bttAutomato;
+    public Button bttGramReg;
+
+    //Funcao chamada quando o bttExpReg é clicado
+    public void irTelaExpReg(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("exp-reg-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Expressões Regulares");
+        stage.show();
+    }
+
+    public void irTelaAutomatos(ActionEvent actionEvent) {
+        System.out.println("Automatos");
+    }
+
+    public void irTelaGramReg(ActionEvent actionEvent) {
+        System.out.println("GramReg");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //System.out.println("HelloController");
     }
 }
